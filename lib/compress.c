8e8bf0d8cb487a5b2ffc30e1f9ad3612a1b57c95
Krizsan0596 <kristof.nagy.lazar@gmail.com>
2025-11-01 19:07:43 +0100

    refactor: Reworked Huffman tree to use array-based storage

--- lib/compress.c
+++ lib/compress.c
@@ -3,21 +3,23 @@
 #include "data_types.h"
 
 static int compare_nodes(const void *a, const void *b) {
-    Node *node_a = *(Node**)a;
-    Node *node_b = *(Node**)b;
-    return (node_a->frequency - node_b->frequency);
+    Node node_a = *(Node*)a;
+    Node node_b = *(Node*)b;
+    return (node_a.frequency - node_b.frequency);
 }
 
-void sort_nodes(Node **nodes, int len) {
-    qsort(nodes, len, sizeof(Node*), compare_nodes);
+void sort_nodes(Node *nodes, int len) {
+    qsort(nodes, len, sizeof(Node), compare_nodes);
 }
 
-int len_frequencies(char *data, long data_len, long *frequencies) {
+int count_frequencies(char *data, long data_len, long *frequencies) {
     for (int i = 0; i < data_len; i++){
         frequencies[(unsigned char) data[i]] += 1;
     }
     return 0;
 }
+
+
 
 Node construct_leaf(long frequency, char data) {
     Node leaf;
@@ -27,46 +29,41 @@ Node construct_leaf(long frequency, char data) {
     return leaf;
 }
 
-Node construct_branch(Node *left, Node *right) {
+Node construct_branch(Node *nodes, int left_index, int right_index) {
     Node branch;
     branch.type = BRANCH;
-    branch.left = left;
-    branch.right = right;
-    branch.frequency = left->frequency + right->frequency;
+    branch.left = left_index;
+    branch.right = right_index;
+    branch.frequency = nodes[left_index].frequency + nodes[right_index].frequency;
     return branch;
 }
 
-Node* construct_tree(Node **nodes, long leaf_count) { // nodes is sorted
+Node* construct_tree(Node *nodes, long leaf_count) { // nodes is sorted
     if (leaf_count <= 0) return NULL;
     if (leaf_count == 1) {
-        return nodes[0];
+        return &nodes[0];
     }
     long current_leaf = 0;
     long current_branch = leaf_count;
     long last_branch = leaf_count;
-    Node *left, *right;
-    while (current_leaf < leaf_count) {
-        if (current_branch == last_branch || nodes[current_leaf]->frequency <= nodes[current_branch]->frequency) {  // branch is empty or leaf < branch
-            left = nodes[current_branch];
-            current_branch++;
-        }
-        else {
-            left = nodes[current_leaf];
-            current_leaf++;
-        }
-        if (current_branch == last_branch || nodes[current_leaf]->frequency <= nodes[current_branch]->frequency) {  // branch is empty or leaf < branch
-            right = nodes[current_branch];
-            current_branch++;
-        }
-        else {
-            right = nodes[current_leaf];
-            current_leaf++;
+
+    for (int i = 0; i < leaf_count - 1; i++) {
+        int left_index, right_index;
+
+        if (current_leaf < leaf_count && (current_branch == last_branch || nodes[current_leaf].frequency <= nodes[current_branch].frequency)) {
+            left_index = current_leaf++;
+        } else {
+            left_index = current_branch++;
         }
 
-        Node *branch = malloc(sizeof(Node));
-        *branch = construct_branch(left, right);
-        nodes[last_branch] = branch;
+        if (current_leaf < leaf_count && (current_branch == last_branch || nodes[current_leaf].frequency <= nodes[current_branch].frequency)) {
+            right_index = current_leaf++;
+        } else {
+            right_index = current_branch++;
+        }
+
+        nodes[last_branch] = construct_branch(nodes, left_index, right_index);
         last_branch++;
     }
-    return nodes[last_branch - 1];
+    return &nodes[last_branch - 1];
 }
--- src/main.c
+++ src/main.c
@@ -6,30 +6,69 @@
 #include "../lib/debugmalloc.h"
 
 int main(int argc, char* argv[]){
-    // for (int i = 0; i < argc; i++){
-    //     if (strcmp(argv[i], "-x") == 0) printf("Extract");
-    //     else if (strcmp(argv[i], "-h") == 0) printf("Usage");
-    //     else printf("Invalid");
-    // }
+    for (int i = 0; i < argc; i++){
+        if (strcmp(argv[i], "-x") == 0) printf("Extract");
+        else if (strcmp(argv[i], "-h") == 0) printf("Usage");
+        else printf("Invalid");
+    }
     char *data;
-    int res = read_raw("test.txt", &data);
+    int data_len = read_raw("test.txt", &data);
+    if (data_len < 0) {
+        fprintf(stderr, "Failed to read file 'test.txt'\n");
+        return 1;
+    }
+
     long *frequencies = calloc(256, sizeof(long));
+    if (frequencies == NULL) {
+        free(data);
+        return 1; // malloc error
+    }
+    count_frequencies(data, data_len, frequencies);
+
     int leaf_count = 0;
     for (int i = 0; i < 256; i++) {
-        if (frequencies[i] != 0) leaf_count++;
+        if (frequencies[i] != 0) {
+            leaf_count++;
+        }
     }
-    Node **nodes = malloc((2 * leaf_count - 1) * sizeof(Node*));
+
+    if (leaf_count == 0) {
+        free(data);
+        free(frequencies);
+        printf("File is empty.\n");
+        return 0;
+    }
+
+    Node *nodes = malloc((2 * leaf_count - 1) * sizeof(Node));
+    if (nodes == NULL) {
+        free(data);
+        free(frequencies);
+        return 1; // malloc error
+    }
+
     int j = 0;
     for (int i = 0; i < 256; i++) {
         if (frequencies[i] != 0) {
-            Node *leaf = malloc(sizeof(Node));
-            *leaf = construct_leaf(frequencies[i], (char) i);
-            nodes[j] = leaf;
+            nodes[j] = construct_leaf(frequencies[i], (char) i);
             j++;
         }
     }
+
     sort_nodes(nodes, leaf_count);
     Node *root_node = construct_tree(nodes, leaf_count);
+
+    if (root_node != NULL) {
+        long tree_size = (root_node - nodes) + 1;
+        Node *resized_nodes = realloc(nodes, tree_size * sizeof(Node));
+        if (resized_nodes != NULL) {
+            nodes = resized_nodes;
+        }
+    }
+
+
+    free(data);
+    free(frequencies);
+    free(nodes);
 
     return 0;
 }
