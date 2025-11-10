3be793214463441008f1c7864f59f56fb33727b1
Krizsan0596 <kristof.nagy.lazar@gmail.com>
2025-11-10 17:56:43 +0100

    Update branch with changes from master.

--- lib/directory.h
+++ lib/directory.h
@@ -4,5 +4,6 @@
 #include "data_types.h"
 
 int archive_directory(char *path, Directory_item **archive, int *current, int *archive_size);
+int extract_directory(char *path, Directory_item **archive, int archive_size, bool force);
 
 #endif // DIRECTORY_H
