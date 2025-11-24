576aac545c04bb615cab8224efcf828375532ca3
Krizsan0596 <kristof.nagy.lazar@gmail.com>
2025-11-24 09:05:30 +0100

    refactor: extract compression and decompression logic
    Move compression and decompression routines from main.c into new
    run_compression and run_decompression functions in compress.c and
    decompress.c. This improves code modularity and readability. Update
    main.c to use these new functions, and update headers accordingly.

--- lib/compress.h
+++ lib/compress.h
@@ -12,6 +12,6 @@ char* check_cache(char leaf, char **cache);
 char* find_leaf(char leaf, Node *nodes, Node *root_node);
 int compress(char *original_data, long data_len, Node *nodes, Node *root_node, char** cache, Compressed_file *compressed_file);
 char* generate_output_file(char *input_file);
-
+int run_compression(char *input_file, char *output_file, bool force, bool output_default);
 
 #endif
